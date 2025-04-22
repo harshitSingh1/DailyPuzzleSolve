e38ade86c0d247edf5ec2ff8a3773a86d6cbe9b2
Harshit Singh <sharshitsingh007@gmail.com>
2025-04-22 08:56:35 +0530

    fast reload

--- src/models/Tool.ts
+++ src/models/Tool.ts
@@ -1,20 +1,32 @@
+// src\models\Tool.ts
 import mongoose from 'mongoose';
 
 const toolSchema = new mongoose.Schema({
-  title: { 
-    type: String, 
-    required: true 
+  title: {
+    type: String,
+    required: true,
+    index: true
   },
-  subheading: { 
-    type: String 
+  subheading: {
+    type: String,
+    index: true
   },
-  image: { 
-    type: String 
+  image: {
+    type: String
   },
-  url: { 
-    type: String, 
-    required: true 
+  url: {
+    type: String,
+    required: true
   }
+});
+
+toolSchema.index({ title: 'text', subheading: 'text', tags: 'text' });
+
+toolSchema.virtual('tags').get(function() {
+  const tagRegex = /#(\w+)/g;
+  const matches = (this.subheading || '').matchAll(tagRegex);
+  const tagsArray = Array.from(matches).map(match => match[1]);
+  return [...new Set(tagsArray)];
 });
 
 const Tool = mongoose.models.Tool || mongoose.model('Tool', toolSchema);
--- src/pages/api/shops.ts
+++ src/pages/api/shops.ts
@@ -7,13 +7,13 @@ export default async function handler(_req: NextApiRequest, res: NextApiResponse
   await dbConnect();
 
   try {
-    const shops = await Shop.find();
+    const shops = await Shop.find().lean();
     res.status(200).json({ success: true, data: shops });
   } catch (error) {
     console.error('Error fetching shop items:', error);
-    res.status(500).json({ 
+    res.status(500).json({
       success: false,
-      error: 'Failed to fetch shop items'
+      error: 'Failed to fetch shop items',
     });
   }
 }
